8b0bd3d1d9aa01c1e2edb8289ad96eab19a6c43b
Shekhov Ismoil <veyxov@gmail.com>
2022-08-15 14:30:25 +0500

    Basic tui todo-app

--- tui-proj/main.go
+++ tui-proj/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+    app := tview.NewApplication()
+	inputField := tview.NewInputField().
+    SetLabel("Enter a task: ").SetFieldWidth(25)
+
+    flex := tview.NewFlex()
+    list := tview.NewList()
+
+    flex.AddItem(list, 50, 0, false);
+    flex.AddItem(inputField, 100, 1, false);
+
+    inputField.
+		SetDoneFunc(func(key tcell.Key) {
+            list.AddItem(inputField.GetText(), "", '-', nil)
+            inputField.SetText("")
+		})
+
+	if err := app.SetRoot(flex, true).SetFocus(inputField).Run(); err != nil {
 		panic(err)
 	}
 }

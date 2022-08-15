3b79031cb6953661b708e2540db84126eb352ace
Shekhov Ismoil <veyxov@gmail.com>
2022-08-15 16:04:27 +0500

    Added file creation deletion read.

--- file-reader/main.go
+++ file-reader/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"log"
+	"fmt"
 	"os"
 )
 
@@ -14,11 +14,47 @@ func printFileLines(file *os.File) {
     }
 }
 
+func openFile(filePath string) (*os.File, error) {
+    file, err := os.Open(filePath)
+
+    return file, err;
+}
+
+func getUserInput(prompt string) string {
+    fmt.Print(prompt)
+
+    var result string
+    fmt.Scanf("%s", &result)
+
+    return result
+}
+
 func main() {
-    file, err := os.Open("file.txt")
-    if err != nil {
-        log.Fatal("Couldn't open file.")
+    for {
+        user_input := getUserInput("Enter cmd (1.read, 2.create, 3.delete): ")
+        path := getUserInput("Enter the path: ")
+        switch user_input {
+            case "1": readFile(path)
+            case "2": createFile(path)
+            case "4": deleteFile(path)
+        }
+        fmt.Println()
     }
-    printFileLines(file)
+}
+
+func deleteFile(path string) error {
+    err := os.Remove(path)
+    return err
+}
+
+func createFile(path string) error {
+    _, err := os.Create(path)
+
+    return err
+}
+
+func readFile(path string) {
+    file, _ := openFile(path)
     defer file.Close()
+    printFileLines(file)
 }

2de75b3e0e2ba12c6a9f522914fb14aa8c74381c
Shekhov Ismoil <veyxov@gmail.com>
2022-08-15 20:48:19 +0500

    First lesson done

---
+++ Alif/lesson1/hw1.go
@@ -0,0 +1,9 @@
+package main;
+
+func main() {
+    println("Hello, I am Shekhov Ismoil.")
+    println()
+    println("I work at Alif-Tech department.")
+    println("I am a .NET back-end developer there.")
+    println("I am found of learning new thechnologies and languages.")
+}

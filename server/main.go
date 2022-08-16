632d86b48eaed835be5032c611cc7ca234116164
Shekhov Ismoil <veyxov@gmail.com>
2022-08-16 14:06:58 +0500

    simple data mirror

--- server/main.go
+++ server/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        log.Print("hello")
+        // Get data from body
+        all, err := ioutil.ReadAll(r.Body);
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        // Print it back to the response.
+        fmt.Fprintf(w, "body: %s", all)
     })
 
 	http.ListenAndServe(":9090", nil);

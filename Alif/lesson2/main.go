0af38ca46e7fe51f3068c9b7e5b243cf30267560
Shekhov Ismoil <veyxov@gmail.com>
2022-08-17 10:00:42 +0500

    solved lesson2 problems.

--- Alif/lesson2/main.go
+++ Alif/lesson2/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "fmt"
+
+func solve1() {
+    var x int
+    fmt.Scan(&x)
+
+    fmt.Printf("The next number for the number %d is %d.\n", x, x + 1)
+    fmt.Printf("The previous number for the number %v is %v.", x, x - 1)
+}
+
 func main() {
-    // TODO: arst
-    // FIX: fixme
-    // NOTE: note
-    println("arst")
+    solve1()
 }

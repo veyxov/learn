71d81e7bc7549f27151e934a8f51d97f38e41797
Shekhov Ismoil <veyxov@gmail.com>
2022-08-30 14:21:52 +0500

    playing with react and material ui

--- savol/front/src/App.tsx
+++ savol/front/src/App.tsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { FC } from "react"
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
+
+interface IQuestionProps {
+    title: string,
+    desc: string
+}
+
+const Question: FC<IQuestionProps> = (props) => {
+    return (
+        <>
+        <Card style={{background:"blue"}}>
+            <CardContent>
+                {props.title}
+            </CardContent>
+            <CardContent>
+            <Typography variant="body2" color="text.secondary">{props.desc}</Typography>
+            </CardContent>
+        </Card>
+        </>
+    )
+}
 
 function App() {
   return (
     <>
-        <h1>Hi</h1>
+    <Grid container spacing={4} style={{background:"gray"}}>
+        <Grid item xs={12}>
+            <Question title={"arst"} desc={"this is arst"} />
+        </Grid>
+        <Grid item xs={12}>
+            <Button>arst</Button>
+        </Grid>
+    </Grid>
     </>
   )
 }

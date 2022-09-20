da7c8a1aaa350399794a9ede33854b4be3a517a8
Shekhov Ismoil <veyxov@gmail.com>
2022-09-20 20:28:30 +0500

    added the linked list classes

---
+++ LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Node<T>
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; set; }
+}
+
+public class LinkedList<T>
+{
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}

eb644d96437081f0d7c1810720fb28addf46c6e5
Shekhov Ismoil <veyxov@gmail.com>
2022-10-01 21:58:13 +0500

    error handling in rust

--- std/src/main.rs
+++ std/src/main.rs
@@ -1,10 +1,15 @@
-struct Person {
-    name: String,
-    age: u8
+fn can_fail(num: i64) -> Result<i64, String> {
+    if num < 0 {
+        return Err("Negative value provided".to_owned());
+    }
+
+    return Ok(num + 1);
 }
 
 fn main() {
-    let ismoil = Person { name: "Ismoil".to_string(), age: 16 };
+    let result = can_fail(7);
+    let result2 = can_fail(-1);
 
-    println!("{} {}", ismoil.name, ismoil.age);
+    println!("{}", result.unwrap_or(0));
+    println!("{}", result2.unwrap_or_default());
 }

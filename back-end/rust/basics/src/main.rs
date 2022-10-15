ccc9f72d84a3c69d0894e1fde81dd048707e4b4f
Shekhov Ismoil <veyxov@gmail.com>
2022-10-15 19:25:22 +0500

    added foreach

--- back-end/rust/basics/src/main.rs
+++ back-end/rust/basics/src/main.rs
@@ -1,7 +1,18 @@
-fn main() {
+fn _borrowing() {
     let first = String::from("Hello world");
     let second = first.clone();
 
     println!("{}", first);
     println!("{}", second);
+}
+
+fn foreach()
+{
+    for number in (1..10).rev() {
+        println!("{number}")
+    }
+}
+
+fn main() {
+    foreach();
 }

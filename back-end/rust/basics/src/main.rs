ce2ed4fcb5263e544118b553dc5b9db0d2a85ba5
Shekhov Ismoil <veyxov@gmail.com>
2022-10-22 20:14:33 +0500

    learn about the option type

--- back-end/rust/basics/src/main.rs
+++ back-end/rust/basics/src/main.rs
@@ -1,2 +1,12 @@
+fn can_be_null(shoud_be_null: bool) -> Option<i32> {
+    if shoud_be_null {
+        return None
+    }
+
+    return Some(112)
+}
+
 fn main() {
+    println!("{:?}", can_be_null(false));
+    println!("{:?}", can_be_null(true));
 }

3d3c38e9d6c598231005e8ce3593b82181ece6ea
Shekhov Ismoil <veyxov@gmail.com>
2022-10-22 20:18:15 +0500

    using the match operator on option type

--- back-end/rust/basics/src/main.rs
+++ back-end/rust/basics/src/main.rs
@@ -9,4 +9,11 @@ fn can_be_null(shoud_be_null: bool) -> Option<i32> {
 fn main() {
     println!("{:?}", can_be_null(false));
     println!("{:?}", can_be_null(true));
+
+    let result = can_be_null(false);
+
+    match result {
+        Some(val) => println!("has value {}", val),
+        None => println!("nothing")
+    }
 }

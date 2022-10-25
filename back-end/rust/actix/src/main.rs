febc78994edfeea176d7baa57d09574c186bbc4e
Shekhov Ismoil <veyxov@gmail.com>
2022-10-25 20:38:39 +0500

    added index

--- back-end/rust/actix/src/main.rs
+++ back-end/rust/actix/src/main.rs
@@ -1,5 +1,10 @@
 #[macro_use] extern crate rocket;
 
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
 #[get("/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
@@ -7,5 +12,5 @@ fn hello(name: &str, age: u8) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![hello])
+    rocket::build().mount("/hello", routes![hello]).mount("/", routes![index])
 }

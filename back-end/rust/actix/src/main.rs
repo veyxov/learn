c134b36b406d3947913ea5ffaf3d1d1612bcdb78
Shekhov Ismoil <veyxov@gmail.com>
2022-10-30 12:02:40 +0500

    added asynchrony tests

--- back-end/rust/actix/src/main.rs
+++ back-end/rust/actix/src/main.rs
@@ -1,3 +1,5 @@
+use rocket::tokio::time::{sleep, Duration};
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -10,7 +12,16 @@ fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {}", seconds)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![hello]).mount("/", routes![index])
+    rocket::build()
+        .mount("/hello", routes![hello])
+        .mount("/", routes![index])
+        .mount("/delay", routes![delay])
 }

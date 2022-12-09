7c0aa779d6fa6b036c88af81985f86996d7ecb4f
Shekhov Ismoil <veyxov@gmail.com>
2022-12-09 09:14:27 +0500

    structs

--- back-end/rust/basics/src/main.rs
+++ back-end/rust/basics/src/main.rs
@@ -1,6 +1,29 @@
+struct Rect {
+    // Sides of the rectangle
+    a: i32,
+    b: i32
+}
+
+impl Rect {
+    // Calculate area:
+    // S = a * b
+    fn calculate_area(&self) -> i32 {
+        self.a * self.b
+    }
+
+    // Calculate perimeter:
+    // P = 2 * (a + b)
+    fn calculate_perimeter(&self) -> i32 {
+        2 * (self.a + self.b)
+    }
+}
+
 fn main() {
-    // Use as many approaches as you can to make it work
-    let x = String::from("hello, world");
-    let y = x.clone();
-    println!("{},{}",x,y);
+    let rect1 = Rect {
+        a: 2,
+        b: 3
+    };
+
+    println!("Rectangle area: {}", rect1.calculate_area());
+    println!("Rectangle perimeter: {}", rect1.calculate_perimeter());
 }
